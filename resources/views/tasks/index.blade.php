e57bfb241f87ec36819e4ab07457697b33bb5f75
EC2 Default User <ec2-user@ip-172-31-5-236.us-east-2.compute.internal>
2018-06-05 04:51:03 +0000

    syusei

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 
- <h1>メッセージ一覧</h1>
+ <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}
-                {!! link_to_route('tasks.create', '新規タスクの追加') !!}</li>
+               </li>
             @endforeach
         </ul>
     @endif
-
+   {!! link_to_route('tasks.create', '新規タスクの追加') !!}
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
- <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
+ <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
     <p>{{ $task->content }}</p>
 

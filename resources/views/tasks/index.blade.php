99fe250ab1beef05b1149fa11184d1c989d0a830
EC2 Default User <ec2-user@ip-172-31-5-236.us-east-2.compute.internal>
2018-06-05 03:59:18 +0000

    CRUD tasks2

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,7 +7,8 @@
     @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
-                <li>{{ $task->content }}</li>
+               <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}
+                {!! link_to_route('tasks.create', '新規タスクの追加') !!}</li>
             @endforeach
         </ul>
     @endif
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,7 +5,7 @@
 
     <p>{{ $task->content }}</p>
 
-    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'タスク編集', ['id' => $task->id]) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
